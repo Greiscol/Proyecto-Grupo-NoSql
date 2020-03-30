2a9c357456cd3a2eb004b0f9d811d6072a089690
Greiscol <60492971+Greiscol@users.noreply.github.com>
2020-03-29 19:15:59 -0600

    Add files via upload

---
+++ MongoExample.Modelo/MisColecciones/Medico.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoExample.Modelo.MisColecciones
+{
+
+    
+    [BsonIgnoreExtraElements]
+    public class Medico
+    {
+        [BsonId]
+        public ObjectId id;
+        [BsonElement("nombre")]
+        public string nombre { get; set; }
+        [BsonElement("años")]
+        public string  edad { get; set; }
+        [BsonElement("especialidad")]
+        public string Especialidad { get; set; }
+        [BsonElement("detalles")]
+        public Detalles LosDetalles { get; set; }
+        [BsonElement("operaciones")]
+        public string[] operaciones { get; set; }
+        [BsonIgnore] 
+        [BsonExtraElements] public BsonDocument Metadata { get; set; }
+    }
+
+    public class Detalles
+    {
+        [BsonElement("residencia")]
+        public string Residencia { get; set; }
+        [BsonElement("provincia")]
+        public string Provincia { get; set; }
+        [BsonExtraElements] public BsonDocument Metadata { get; set; }
+
+    }
+}
---
+++ MongoExample.PruebaConsola/InvocadorMedico.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver;
+using MongoExample.Modelo.MisColecciones;
+using MongoExample.Negocio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoExample.PruebaConsola
+{
+    class InvocadorMedico
+    {
+        private string nombreMedico;
+        public void ConsultarMedicos()
+        {
+            CapturarInformacion();
+            var listaMedicos = EjecutarConsulta(nombreMedico);
+            ImprimirListaDeMedicos(listaMedicos);
+        }
+
+        private void ImprimirListaDeMedicos(IList<Medico> listaMedicos)
+        {
+            if (listaMedicos.Count > 0)
+            {
+                foreach (var item in listaMedicos)
+                {
+                    Console.WriteLine("Nombre: {0}  - Edad: {1} - Especialidad: {2}", item.nombre, item.edad, item.Especialidad);
+                }
+                string response = string.Empty;
+                Console.Write("Desea consultar otro medico: ");
+                response = Console.ReadLine();
+                if (response=="yes")
+                {
+                    ConsultarMedicos();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron medicos.");
+                ConsultarMedicos();
+            }
+            ;
+            Console.ReadLine();
+        }
+
+        private IList<Modelo.MisColecciones.Medico> EjecutarConsulta(string nombreMedico)
+        {
+            var elConsultante = new Medicos();
+            var elResultado = elConsultante.ListarMedicosPorNombre(nombreMedico);
+            return elResultado;
+        }
+
+        private void CapturarInformacion()
+        {
+            Console.Write("Digite el nombre del medico: ");
+            nombreMedico = Console.ReadLine();
+        }
+    }
+}
